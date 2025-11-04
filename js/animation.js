5b50967f68f660c0eb5ecfd827fb6a45146fe7ba
pishkawebproduction-cloud <pishka.web.production@gmail.com>
2025-11-04 20:25:24 +0900

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -549,12 +549,12 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!spMenuPic) return;
 
   const images = [
-    "images/cheeseCake.jpg",
-    "images/chocoCake.jpg",
-    "images/cupCake.jpg",
-    "images/fancyCake.jpg",
-    "images/tart.jpg",
-    "images/cupCake.jpg"
+    "images/cheeseCake.webp",
+    "images/chocoCake.webp",
+    "images/cupCake.webp",
+    "images/fancyCake.webp",
+    "images/tart.webp",
+    "images/cupCake.webp"
   ];
   let index = 0;
 
